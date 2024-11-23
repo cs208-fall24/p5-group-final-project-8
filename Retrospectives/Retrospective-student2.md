06cdbd5bfdebc92bc38528a17b8a9cf137bf7e42
wilsoninthewoods <129208765+wilsoninthewoods@users.noreply.github.com>
2024-11-22 22:53:17 -0700

    Finished my portion of the site. Fixed comment section and added classes.

--- Retrospectives/Retrospective-student2.md
+++ Retrospectives/Retrospective-student2.md
@@ -1,16 +1,29 @@
 # Retrospective
 
-- name: Jane Doe
-- email: janedoe@u.boisestate.edu
+- name: Joshua D Wilson
+- email: JoshuaDWilson@u.boisestate.edu
 
 ## Experience
 
-TODO
+This project was fun but difficult. I’m used to solving physical, hands-on problems, so sitting down and tackling something like building a website was definitely a challenge. It was pretty satisfying to see everything come together, almost like finishing a house—it just happens to be digital.
+
+The hardest part for me was getting used to the syntax and tools. Learning how to structure the CSS and make everything look decent was kind of like laying out a set of building plans: you’ve got to think ahead and make sure all the pieces work together. The most rewarding part, though, was getting the comment section to work. When I saw that first comment appear on the page, I felt like I’d just hung the last cabinet in a kitchen remodel—super satisfying.
+
+One thing that stood out to me is how similar this is to managing a team on a job site. There’s communication, planning, and making sure everyone’s work fits together in the end. I guess construction and coding have more in common than I thought!
 
 ## Known issues or Bugs
 
-TODO
+At first, the comments weren’t showing up on the page. Turned out, I wasn’t querying the database correctly. Once I figured that out, it worked like a charm.
+Responsiveness on mobile was tricky. Some elements didn’t line up right at smaller screen sizes until I added a few media queries. It’s all good now, though.
+
+My biggest issue is with Node.js my computer is taking a ridiculous amount of time to start the server. I have tried to fix it but I have not been able to do so. 
 
 ## Sources used
+https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/addressing-merge-conflicts/resolving-a-merge-conflict-on-github
+https://github.com/orgs/community/discussions/26304
+https://www.youtube.com/watch?v=-e3eg2Dnez4
+https://www.codecademy.com/learn/connecting-javascript-and-sql/modules/learn-node-sqlite-module/cheatsheet
 
-TODO
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,38 +1,75 @@
-import express from 'express'
-import sql from 'sqlite3'
+import express from 'express';
+import sql from 'sqlite3';
 
-const sqlite3 = sql.verbose()
+const sqlite3 = sql.verbose();
 
-// Create an in memory table to use
-const db = new sqlite3.Database(':memory:')
+// Create an in-memory database
+const db = new sqlite3.Database(':memory:');
 
-const app = express()
-app.use(express.static('public'))
-app.set('views', 'views')
-app.set('view engine', 'pug')
-app.use(express.urlencoded({ extended: false }))
+// Initialize the database table
+db.serialize(() => {
+  db.run('CREATE TABLE underwater_welding (id INTEGER PRIMARY KEY, comment TEXT)');
+});
 
-app.get('/', function (req, res) {
-  console.log('GET called')
-  res.render('index')
-})
+const app = express();
+app.use(express.static('public'));
+app.set('views', 'views');
+app.set('view engine', 'pug');
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/student1', function (req, res) {
-  console.log('GET called')
-  res.render('student1')
-})
+// Routes
 
-app.get('/student2', function (req, res) {
-  console.log('GET called')
-  res.render('student2')
-})
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-app.get('/student3', function (req, res) {
-  console.log('GET called')
-  res.render('student3')
-})
+app.get('/student1', (req, res) => {
+  res.render('student1');
+});
+
+app.get('/student2', (req, res) => {
+  db.all('SELECT id, comment FROM underwater_welding LIMIT 5', (err, rows) => {
+    if (err) {
+      console.error('Error retrieving comments:', err);
+      return res.status(500).send('Server error while retrieving comments.');
+    }
+    res.render('student2', { comments: rows });
+  });
+});
+
+app.post('/student2/add-comment', (req, res) => {
+  const { comment } = req.body;
+  if (!comment) {
+    return res.status(400).send('Comment cannot be empty.');
+  }
+  db.run('INSERT INTO underwater_welding (comment) VALUES (?)', [comment], (err) => {
+    if (err) {
+      console.error('Error adding comment:', err);
+      return res.status(500).send('Server error while adding comment.');
+    }
+    res.redirect('/student2');
+  });
+});
+
+app.post('/student2/delete-comment', (req, res) => {
+  const { id } = req.body;
+  if (!id) {
+    return res.status(400).send('Invalid comment ID.');
+  }
+  db.run('DELETE FROM underwater_welding WHERE id = ?', [id], (err) => {
+    if (err) {
+      console.error('Error deleting comment:', err);
+      return res.status(500).send('Server error while deleting comment.');
+    }
+    res.redirect('/student2');
+  });
+});
+
+app.get('/student3', (req, res) => {
+  res.render('student3');
+});
 
 // Start the web server
-app.listen(3000, function () {
-  console.log('Listening on port 3000...')
-})
+app.listen(3000, () => {
+  console.log('Listening on port 3000...');
+});
